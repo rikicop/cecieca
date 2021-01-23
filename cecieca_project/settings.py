6c52f6619d3581daae8006813db1ffcbfaff4b66
rikicop <ruperto1@protonmail.com>
2021-01-23 14:48:37 -0400

    heroku config

--- cecieca_project/settings.py
+++ cecieca_project/settings.py
@@ -1,6 +1,9 @@
 
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cecieca_project.urls'
@@ -117,6 +121,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #NEW
+
+django_heroku.settings(locals()) #NEW
 
 #https://myaccount.google.com/lesssecureapps
 
