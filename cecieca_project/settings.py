de92bcd08a15f4e668c173c7fbc3542a449cdc9b
rikicop <ruperto1@protonmail.com>
2021-01-23 14:26:58 -0400

    new migrations

--- cecieca_project/settings.py
+++ cecieca_project/settings.py
@@ -66,11 +66,14 @@ WSGI_APPLICATION = 'cecieca_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cecieca_db',
+        'USER': 'cecieca_user',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
